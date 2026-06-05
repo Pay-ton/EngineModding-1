5b470f34a524e07d9b5e3876c1fab187fad73cc5
Pay-ton <001224922@mohawkcollege.ca>
2026-06-05 11:19:51 -0400

    Level Loading functioning

---
+++ EngineModding-2D-Platformer/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,20 @@
+using Unity.VectorGraphics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Collider2D LevelCollider;
+    public string LevelName; 
+    public void LoadConditionTriggered() 
+    {
+        SceneManager.LoadScene(LevelName,LoadSceneMode.Single);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        LoadConditionTriggered();
+    }
+
+}
