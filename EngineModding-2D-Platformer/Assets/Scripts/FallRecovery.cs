c29cc00ecee07f3439b08a1fd460cfa937e0705a
Pay-ton <paytonbontje@gmail.com>
2026-06-08 15:35:34 -0400

    new non-scalable additions to Fall Recovery script allowing for multiple recovery points, but please (self/any onlookers) do not use this for more than 3. This is a horrible solution.

--- EngineModding-2D-Platformer/Assets/Scripts/FallRecovery.cs
+++ EngineModding-2D-Platformer/Assets/Scripts/FallRecovery.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class FallRecovery : MonoBehaviour
 {
+    // Allows setting recovery point in Unity
+    // I suck at arrays, I know theres a way to make a recovery point array with []
+    // but I have no clue how, so Im just going to take the lazy approach because I only need 3 recovery triggers. 
     public Transform RecoveryPoint1;
+    public Transform RecoveryPoint2;
+    public Transform RecoveryPoint3;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Turn the Collider into a trigger for this script to transform location to associated recovery point.
+        // So long as only one recovery point is set, this will just check for all 3 and respawn the player accordingly.
+        // Yes it isn't scalable, I'm bad at coding.
         collision.gameObject.transform.position = RecoveryPoint1.position;
+        collision.gameObject.transform.position = RecoveryPoint2.position;
+        collision.gameObject.transform.position = RecoveryPoint3.position;
     }
 }
