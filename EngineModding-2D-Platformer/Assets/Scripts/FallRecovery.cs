0a3d5a8bd4901e995d56dcf86486ed6e5afafe16
Pay-ton <paytonbontje@gmail.com>
2026-06-10 22:10:40 -0400

    The fall script was inherently scaled, I made unnecessary additions. Cutting unnecessary code.

--- EngineModding-2D-Platformer/Assets/Scripts/FallRecovery.cs
+++ EngineModding-2D-Platformer/Assets/Scripts/FallRecovery.cs
@@ -6,16 +6,16 @@ public class FallRecovery : MonoBehaviour
     // I suck at arrays, I know theres a way to make a recovery point array with []
     // but I have no clue how, so Im just going to take the lazy approach because I only need 3 recovery triggers. 
     public Transform RecoveryPoint1;
-    public Transform RecoveryPoint2;
-    public Transform RecoveryPoint3;
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Turn the Collider into a trigger for this script to transform location to associated recovery point.
         // So long as only one recovery point is set, this will just check for all 3 and respawn the player accordingly.
         // Yes it isn't scalable, I'm bad at coding.
+
+        // APPARENTLY IT WAS ALWAYS INHERENTLY SCALED, IM JUST DUMB.
         collision.gameObject.transform.position = RecoveryPoint1.position;
-        collision.gameObject.transform.position = RecoveryPoint2.position;
-        collision.gameObject.transform.position = RecoveryPoint3.position;
+    
     }
 }
